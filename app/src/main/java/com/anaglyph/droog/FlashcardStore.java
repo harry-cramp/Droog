501b4a038b000cb8262861ec7e995d9dd8403739
Harry Cramp <harrycramp@hotmail.com>
2020-11-07 12:54:11 +0000

    Create system for storing flashcard data in memory

--- app/src/main/java/com/anaglyph/droog/FlashcardStore.java
+++ app/src/main/java/com/anaglyph/droog/FlashcardStore.java
@@ -1,4 +1,34 @@
 package com.anaglyph.droog;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FlashcardStore {
+
+    public static ArrayList<WordPair> wordPairs = new ArrayList<WordPair>();
+
+    public static String getAnswer(String word) {
+        for(WordPair pair : wordPairs) {
+            if(pair.getFirstWord().equals(word))
+                return pair.getSecondWord();
+            else if(pair.getSecondWord().equals(word))
+                return pair.getFirstWord();
+        }
+
+        return null;
+    }
+
+    public static void putWordPair(String firstWord, String secondWord, String hint) {
+        wordPairs.add(new WordPair(firstWord, secondWord, hint));
+    }
+
+    public static String getHint(String word) {
+        for(WordPair pair : wordPairs) {
+            if(pair.getFirstWord().equals(word) || pair.getSecondWord().equals(word))
+                return pair.getHint();
+        }
+
+        return null;
+    }
+
 }
