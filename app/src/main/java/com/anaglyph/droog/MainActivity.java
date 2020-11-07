8e32b2e55bc321f2cd82beb150f86288e3e8e60d
Harry Cramp <harrycramp@hotmail.com>
2020-11-07 13:44:48 +0000

    Add menu button to load new flashcard screen

--- app/src/main/java/com/anaglyph/droog/MainActivity.java
+++ app/src/main/java/com/anaglyph/droog/MainActivity.java
@@ -1,8 +1,13 @@
 package com.anaglyph.droog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Context context = this.getApplicationContext();
+        final Button newFlashcardButton = (Button)findViewById(R.id.menuNewFlashcardButton);
+        newFlashcardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, NewFlashcard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
