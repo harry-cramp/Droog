a9c0073e56bd11f8804cc96a67348f76371c9f87
Harry Cramp <harrycramp@hotmail.com>
2020-11-12 19:07:05 +0000

    Fix variable name spelling mistake

--- app/src/main/java/com/anaglyph/droog/QuizQuestionFragment.java
+++ app/src/main/java/com/anaglyph/droog/QuizQuestionFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 public class QuizQuestionFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceStatex) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.question_fragment, container, false);
     }
 
